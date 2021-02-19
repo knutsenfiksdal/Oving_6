145b7abe089b917921e94404642f3cf762f4a080
Marius <marius@fiksdal.io>
2021-02-19 03:08:59 +0100

    Added more documentation

--- Oving_6_V1.py
+++ Oving_6_V1.py
@@ -1,3 +1,9 @@
+"""
+Marius Fiksdal
+marius@fiksdal.io
+14.02.2021
+"""
+
 from enkelt_kortspill_fullstendig import Kortstokk, Kort
 
 
@@ -11,23 +17,25 @@ class Spill:
             self.plasser.append([self.kortstokk.trekk()])
 
     def skriv_tilstand(self):
+        """Skriver ut de fremste kortene og antall kort i kortstokken"""
         print("˅------KORT------˅")
         j = 0
         for i in self.plasser:
             print(f"{j}: {i[-1]}")
             j += 1
         print("˄------KORT------˄")
-
         print(f"Antall kort: {len(self.kortstokk)}")
         # return f"Kort som ligger: {}Antall kort: {len(self.kortstokk)}"
 
     def er_ferdig(self):
+        """Sjekker om kortstokken er 'tom'"""
         if len(self.kortstokk) < 2:
             return True
         else:
             return False
 
     def plasser_to_kort(self, kort_1, kort_2):
+        """Sjekker om totalen er 11 og legger to kort hvis dette er sant"""
         to_kort_total = self.plasser[int(kort_1)][-1].verdi + self.plasser[int(kort_2)][-1].verdi
         if to_kort_total == 11:
             self.plasser[int(kort_1)].append(self.kortstokk.trekk())
@@ -37,6 +45,7 @@ class Spill:
             print(f"Summen er ikke 11, den er:{to_kort_total}")
 
     def plasser_tre_kort(self, kort_1, kort_2, kort_3):
+        """Sjekker om disse 3 kortene er bildekort, og legger 3 kort hvis dette er sant"""
         if any(ele in str(self.plasser[int(kort_1)][-1]) for ele in ["Konge", "Knekt", "Dame"]) and any(
                 ele in str(self.plasser[int(kort_2)][-1]) for ele in ["Konge", "Knekt", "Dame"]) and any(
                 ele in str(self.plasser[int(kort_3)][-1]) for ele in ["Konge", "Knekt", "Dame"]):
@@ -49,6 +58,7 @@ class Spill:
 
 
 if __name__ == "__main__":
+    """Kodeblokk som er beskrevet i oppgaveteksten"""
     spel = Spill()
     while not spel.er_ferdig():
         spel.skriv_tilstand()
@@ -65,28 +75,3 @@ if __name__ == "__main__":
 
 
 
-
-
-"""
-spel = Spill()
-spel.skriv_tilstand()
-print()
-
-print(spel.kortstokk.neste_kort().verdi)
-
-spel.plasser_to_kort(2, 3)
-
-spel.plasser_tre_kort(0, 1, 2)
-
-
-        if self.kortstokk.neste_kort().verdi < to_kort_total:
-            pass
-            
-            
-str(self.plasser[int(kort_1)][0]) in ["Konge", "Knekt", "Dame"]:
-
-
-        spel.plasser_to_kort(input("Skriv inn første indeks: "), input("Skriv inn andre indeks: "))
-
-
-"""
